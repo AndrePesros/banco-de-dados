a46278a7f6da45ceccae2765b5790c3c51945256
Aluno <Aluno@LAB2-17>
2021-10-25 16:42:21 -0300

    Adicionar arquivos de projeto.

---
+++ ConfigDb/MyDBContext1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp1.Model;
+
+namespace WindowsFormsApp1.ConfigDb
+{
+    class MyDBContext1: DbContext
+    {
+        DbSet<Evento> Eventos { get; set; }
+    }
+}
